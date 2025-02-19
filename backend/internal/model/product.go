82693028d5b61b5e69b54794346d5d9f6a203a72
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2025-02-20 00:31:06 +1100

    Add json annotations for product struct

--- backend/internal/model/product.go
+++ backend/internal/model/product.go
@@ -1,8 +1,8 @@
 package model
 
 type Product struct {
-	SKU       string  `db:"sku"`
-	Name      string  `db:"name"`
-	Price     float64 `db:"price"`
-	Inventory int     `db:"inventory"`
+	SKU       string  `db:"sku" json:"sku"`
+	Name      string  `db:"name" json:"name"`
+	Price     float64 `db:"price" json:"price"`
+	Inventory int     `db:"inventory" json:"inventory"`
 }
